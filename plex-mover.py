03ae20bb279f8c1ed7ecdf1291af51aa0efaef62
Sarunas Aliskevicius <sarunas.aliskevicius@gmail.com>
2017-09-02 18:48:41 +0300

    ADD Started developing the script

--- plex-mover.py
+++ plex-mover.py
@@ -1,1 +1,15 @@
-print("hello")
+#! /usr/bin/env python
+
+import sys, os, shutil
+
+if len(sys.argv) != 3:
+    sys.exit('You need to specify the source directory and target destination')
+
+source = sys.argv[1]
+target = sys.argv[2]
+
+if os.path.isdir(source) == False:
+    sys.exit('Source directory not found')
+if os.path.isdir(target) == False:
+    sys.exit('Target directory not found')
+
